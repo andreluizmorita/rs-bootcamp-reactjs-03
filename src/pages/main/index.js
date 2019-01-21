717f44dfb7c02894c59f724a1830aa3282ccd4d4
André Luiz Morita <andreluizmorita@gmail.com>
2019-01-21 19:42:27 -0200

    Busca e lista

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-const Main = () => <h1>Main</h1>;
+class Main extends Component {
+  state = {
+    repositoryInput: ""
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuario/repositorio"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+
+        <ul>
+          <li>
+            <p>
+              <strong>facebook/react</strong> (A declarative, efficient, and
+              flexible JavaScript library for building user interfaces.)
+            </p>
+            <a href="https://github.com/facebook/react">Acessar</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
 
 export default Main;
